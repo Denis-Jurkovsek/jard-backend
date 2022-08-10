a6409e82584a09af20fe2fd8d75fd65703f7ab6a
Denis Jurkovsek <denisjurkovsek@gmail.com>
2022-08-10 03:36:28 +0200

    Init

---
+++ src/api/tabacco/routes/tabacco.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tabacco router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tabacco.tabacco');
---
+++ src/api/tabacco/services/tabacco.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tabacco service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tabacco.tabacco');
